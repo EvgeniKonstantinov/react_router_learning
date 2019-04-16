eabe83c2123256d23a05e429111e08e456ada0da
Konstantinov Evgeny <Evgeny.Konstantinov@itspro.by>
2019-04-16 16:44:51 +0300

    create new app

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+const About = () => {
+  return <h2>About</h2>;
+};
 
+const Topic = ({ match }) => {
+  return <h3>Requested Param: {match.params.id}</h3>;
+};
+
+const Home = () => <div>Home</div>;
+
+const App = () => {
+  const someVariable = true;
+
+  return (
+    <Switch>
+      {/* these are good */}
+      <Route exact path="/" component={Home} />
+      <Route
+        path="/about"
+        render={props => <About {...props} extra={someVariable} />}
+      />
+    </Switch>
+  );
+};
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter, Route, Link } from "react-router-dom";
+import * as serviceWorker from "./serviceWorker";
+import loadable from "@loadable/component";
+import Loading from "./Loading";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const LoadableComponent = loadable(() => import("./Dashboard"), {
+  fallback: Loading
+});
+
+export default class LoadableDashboard extends React.Component {
+  render() {
+    return <LoadableComponent />;
+  }
+}
+
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
